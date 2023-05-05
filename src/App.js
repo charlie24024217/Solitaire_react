e650beb1c7299cc0d98f20317768cea26191a1d3
Bagel <abc24024217@gmail.com>
2023-05-05 14:39:14 +0800

    create react project_02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "@emotion/styled";
+
+const theme = {
+  black: "#222222",
+  yellow: "#FFB53E",
+  white: "#FCFCFC",
+  grey: "E2E2E2",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="PlacementArea"></div>
+      <div className="CardAera"></div>
+      <div className="Menu"></div>
     </div>
   );
 }
